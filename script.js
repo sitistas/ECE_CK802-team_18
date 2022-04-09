26eb68ada4aee204a5a500a8332c23032f2fd55e
sitistas <kon.sitistas@gmail.com>
2022-04-09 21:52:16 +0300

    Created script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function popupForm() {
+        document.getElementById("sign-in").style.display = "block";
+        document.getElementById("dim").style.display = "block"
+    }
+    function closeForm() {
+        document.getElementById("sign-in").style.display = "none";
+        document.getElementById("dim").style.display = "none"
+    }

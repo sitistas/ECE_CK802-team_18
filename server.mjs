666b49ee58fd046310484927373abb1ebaec9b52
sitistas <kon.sitistas@gmail.com>
2022-06-02 15:12:25 +0300

    Removed funcs from server.mjs

--- server.mjs
+++ server.mjs
@@ -61,24 +61,3 @@ app.get("/signup", (req, res) => {
     console.log("GET / session=", req.session);
     res.render("signup");
 })
-
-
-
-function popupForm() {
-    document.getElementById("sign-in").style.display = "block";
-    document.getElementById("dim").style.display = "block"
-}
-function closeForm() {
-    document.getElementById("sign-in").style.display = "none";
-    document.getElementById("dim").style.display = "none"
-}
-
-function popupSearch() {
-    document.getElementById("search-bar").style.display = "block";
-    document.getElementById("dim").style.display = "block";
-}
-
-function closeSearch() {
-    document.getElementById("search-bar").style.display = "none";
-    document.getElementById("dim").style.display = "none";
-}

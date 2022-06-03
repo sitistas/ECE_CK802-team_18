a9edcafc7396034850ccfe2d62314fe3e81760f3
sitistas <kon.sitistas@gmail.com>
2022-06-03 21:07:48 +0300

    Trash bro trash

--- db.heroku-pg.js
+++ db.heroku-pg.js
@@ -3,7 +3,7 @@
 const { Client } = require('pg');
 
 const client = new Client({
-    connectionString: 'postgres://lghdvjgzpzukla:d92b59d81b2d067c8b427caaebd5236cae1c14cf8848dccac6ade17294a7ef93@ec2-52-18-116-67.eu-west-1.compute.amazonaws.com:5432/d18j9v5aqokkub',
+    connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false
     }

17454e5f9d9a1aa85391dbc274dc1f69ae934d70
Foivos <phoebus.chal@gmail.com>
2022-06-03 17:41:31 +0300

    Pop up login

--- server.mjs
+++ server.mjs
@@ -22,6 +22,8 @@ const redirectHome = (req, res, next) => {
     }
 };
 
+let returnTo="/";
+
 console.log(process.env.PORT)
 // Εκκίνηση του εξυπηρετητή
 const PORT = process.env.PORT || 8000
@@ -31,9 +33,10 @@ app.listen(PORT, () => {
 
 
 app.get("/", (req, res) => {
+    returnTo = req.originalUrl;
     console.log("GET / session=", req.session);
     console.log(req.query)
-    res.render("index");
+    res.render("index", {layout: checkAuthenticated(req)?"main-logged-in":"main"});
 })
 
 app.get("/signup", (req, res) => {
@@ -43,26 +46,28 @@ app.get("/signup", (req, res) => {
 
 app.get("/publish", (req, res) => {
     if (checkAuthenticated(req) == false) {
-        req.session.returnTo = req.originalUrl;
+        returnTo = req.originalUrl;
         res.redirect("/login");
     }
     else {
         console.log("GET / session=", req.session);
-        res.render("publish");
+        res.render("publish", {layout: checkAuthenticated(req)?"main-logged-in":"main"});
     }
 })
 
 app.get("/admin", (req, res) => {
     console.log("GET / session=", req.session);
-    res.render("admin");
+    res.render("admin", {layout:"main-logged-in"});
 })
 
 app.get("/best-sellers", (req, res) => {
+    returnTo = req.originalUrl;
     console.log("GET / session=", req.session);
-    res.render("best-sellers");
+    res.render("best-sellers", {layout: checkAuthenticated(req)?"main-logged-in":"main"});
 })
 
 app.get("/signup", (req, res) => {
+    returnTo = req.originalUrl;
     console.log("GET / session=", req.session);
     res.render("signup");
 })
@@ -75,10 +80,11 @@ app.get("/login", (req, res) => {
 
 app.get('/auth', (req, res) => {
     console.log(req.query.password)
+    console.log('test');
     if (req.query.password == '123') {
         req.session.loggedUserId = 1;
-        app.engine('.hbs', engine({ extname: '.hbs', defaultLayout: 'main-logged-in' }));
-        res.redirect(req.session.returnTo)
+        // app.engine('.hbs', engine({ extname: '.hbs', defaultLayout: 'main-logged-in' }));
+        res.redirect(returnTo)
     }
     else {
         res.render("login");
@@ -88,12 +94,13 @@ app.get('/auth', (req, res) => {
 app.get("/logout", (req, res) => {
     // console.log(req);
     // console.log("GET / session=", req.session);
-    app.engine('.hbs', engine({ extname: '.hbs', defaultLayout: 'main' }));
+    // app.engine('.hbs', engine({ extname: '.hbs', defaultLayout: 'main' }));
     req.session.destroy();
-    res.redirect('/');
+    res.redirect(returnTo);
 })
 
 app.get('/book/:title', (req, res) => {
+    returnTo = req.originalUrl;
     let bookURL = '/book/' + req.params.title;
     console.log(bookURL);
     res.render('book', { title: req.params.title });

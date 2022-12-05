d1adca6b1f3f33bc066150f08201f8969cffc1e8
Konstantinos Sitistas <kon.sitistas@gmail.com>
2022-12-05 04:36:20 +0200

    check without ssl

--- db.heroku-pg.js
+++ db.heroku-pg.js
@@ -4,10 +4,15 @@ const { Client } = require('pg');
 
 const client = new Client({
     connectionString: process.env.DATABASE_URL,
-    // ssl: {
-    //     rejectUnauthorized: false
-    // }
+    ssl: false,
 });
+
+// const client = new Client({
+//     connectionString: process.env.DATABASE_URL,
+//     // ssl: {
+//     //     rejectUnauthorized: false
+//     // }
+// });
 
 client.connect((err) => {
     if (err)
